31c02e4f9bb7b3c4db9160ce388bf62bf612983e
Alberto Medeiros <b3tomedeiros@gmail.com>
2024-07-20 10:08:50 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,13 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.println("Print piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
